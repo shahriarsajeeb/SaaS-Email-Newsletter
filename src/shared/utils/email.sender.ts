18231d0b09abac0742545b5d6e5dfcbf9cf168e4
shahriarsajeeb <programmershahriarsajeeb@gmail.com>
2024-02-26 01:51:57 +0600

    done

--- src/shared/utils/email.sender.ts
+++ src/shared/utils/email.sender.ts
@@ -9,8 +9,8 @@ interface Props {
 }
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_KEY_ID,
   region: "us-east-1",
 });
 
